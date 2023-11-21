42d06b3226e08e4740f43f1da22024fda69533d2
benjamin <345831755@qq.com>
2023-11-21 18:12:05 +0800

    collider

--- assets/script/BgControl.ts
+++ assets/script/BgControl.ts
@@ -1,20 +1,36 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Collider, Collider2D, Component, Contact2DType, Node, PhysicsSystem2D } from 'cc';
+import { EnemyControl } from './EnemyControl';
+import { BulletControl } from './BulletControl';
 const { ccclass, property } = _decorator;
 
 @ccclass('BgControl')
 export class BgControl extends Component {
     start() {
-
+        PhysicsSystem2D.instance.on(
+            Contact2DType.BEGIN_CONTACT,
+            this.onBeginContact,
+            this
+        )
     }
 
     update(deltaTime: number) {
-        for(let item of this.node.children) {
+        for (let item of this.node.children) {
             const { x, y } = item.getPosition()
             const moveY = y - 100 * deltaTime
             item.setPosition(x, moveY)
-            if(moveY < -870) {
+            if (moveY < -870) {
                 item.setPosition(x, moveY + 852 * 2)
             }
+        }
+    }
+
+    onBeginContact(self: Collider2D, other: Collider2D) {
+        if (other.tag === 1 && self.tag === 0) {
+            other.getComponent(EnemyControl).die()
+            self.getComponent(BulletControl).die()
+        } else if (other.tag === 0 && self.tag === 1) {
+            other.getComponent(BulletControl).die()
+            self.getComponent(EnemyControl).die()
         }
     }
 }
--- assets/script/BulletControl.ts
+++ assets/script/BulletControl.ts
@@ -3,18 +3,28 @@ const { ccclass, property } = _decorator;
 
 @ccclass('BulletControl')
 export class BulletControl extends Component {
+    isDead: boolean = false
     start() {
 
     }
 
     update(deltaTime: number) {
+        if (this.isDead) return
         const { x, y } = this.node.getPosition();
-        const moveY = y + 600 * deltaTime;
+        const moveY = y + 500 * deltaTime;
         this.node.setPosition(x, moveY);
         // 判断超出屏幕销毁子弹
         if (moveY > 800) {
             this.node.destroy();
         }
+    }
+
+    die() {
+        if (this.isDead) return
+        this.isDead = true
+        setTimeout(() => {
+            this.node?.destroy?.()
+        }, 10);
     }
 }
 
