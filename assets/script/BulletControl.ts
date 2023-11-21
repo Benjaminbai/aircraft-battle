898a070090abde3ae35834d6910748b3ea806697
benjamin <345831755@qq.com>
2023-11-21 17:02:45 +0800

    realize small game through cocos

---
+++ assets/script/BulletControl.ts
@@ -0,0 +1,20 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('BulletControl')
+export class BulletControl extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        const { x, y } = this.node.getPosition();
+        const moveY = y + 600 * deltaTime;
+        this.node.setPosition(x, moveY);
+        // 判断超出屏幕销毁子弹
+        if (moveY > 800) {
+            this.node.destroy();
+        }
+    }
+}
+
---
+++ assets/script/PlayerControl.ts
@@ -0,0 +1,34 @@
+import {
+    _decorator,
+    Component,
+    EventTouch,
+    instantiate,
+    Node,
+    Prefab,
+    v3,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+@ccclass("PlayerControl")
+export class PlayerControl extends Component {
+    @property(Prefab)
+    bullet: Prefab = null;
+
+    start() {
+        // 鼠标拖动
+        this.node.on(Node.EventType.TOUCH_MOVE, (e: EventTouch) => {
+            const { x, y } = e.getUILocation();
+            this.node.setWorldPosition(v3(x, y));
+        });
+
+        // 创建子弹
+        this.schedule(() => {
+            const { x, y } = this.node.getPosition();
+            const node = instantiate(this.bullet);
+            node.setParent(this.node.parent);
+            node.setPosition(x, y + 70);
+        }, 0.2);
+    }
+
+    update(deltaTime: number) { }
+}
